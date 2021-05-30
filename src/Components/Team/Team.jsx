a6668cf688833fc1dc19e3eb8c7189fd6804d683
GunjanSingh1 <51900100+GunjanSingh1@users.noreply.github.com>
2021-05-30 19:06:45 +0530

    updates

--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
 import "./team.scss";
 import {
-  KeyboardArrowUp,
+  KeyboardArrowDown,
+  KeyboardDropUp,
   Home,
   Twitter,
   LinkedIn,
   GitHub,
   Mail,
 } from "@material-ui/icons";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import db from "../../firebase";
 
 function Team() {
@@ -49,21 +52,61 @@ function Team() {
       });
   }, []);
 
-  /*const changeViewCollab = () => {
+  const changeViewFaculty = () => {
+    if (nFaculty) {
+      setNFaculty(false);
+    } else {
+      setNFaculty(true);
+    }
+  };
+    const changeViewPhD = () => {
+    if (nPHD) {
+      setNPhd(false);
+    } else {
+      setNPhd(true);
+    }
+  };
+  const changeViewCollab = () => {
     if (viewCollab) {
       setViewCollab(false);
     } else {
       setViewCollab(true);
     }
-  };*/
-
+  };
+  const changeViewAlumni = () => {
+    if (nAlumni) {
+      setNAlumni(false);
+    } else {
+      setNAlumni(true);
+    }
+  };
+  const changeViewBtech = () => {
+    if (nBtech) {
+      setNBtech(false);
+    } else {
+      setNBtech(true);
+    }
+  };
+  const changeViewMtech = () => {
+    if (nMtech) {
+      setNMTECH(false);
+    } else {
+      setNMTECH(true);
+    }
+  };
   return (
+ 
+
     <div className="team">
       <div className="Team-wrapper">
         <div className="title">Meet Our Team</div>
-        {nFaculty ? (
+        <div className="row">
+		<div className="title" onClick={changeViewFaculty}> Faculty {"  "}
+		{nFaculty ? (  <ExpandLessIcon style={{ color: "#a63f04", width: 50}}  /> ) : ( <ExpandMoreIcon style={{ color: "#a63f04", width: 50 }} /> )}
+        </div>
+		</div>
+		{nFaculty ? (
           <div className="row">
-            <div className="title">Faculty</div>
             <div className="profiles">
               {allMembers
                 ?.filter(({ id, member }) => member.Position === "Faculty")
@@ -147,9 +190,13 @@ function Team() {
         ) : (
           ""
         )}
+		<div className="row">
+		<div className="title" onClick={changeViewPhD}> Ph. D Students {"  "}
+		{nPHD ? (  <ExpandLessIcon style={{ color: "#a63f04", width: 50}}  /> ) : ( <ExpandMoreIcon style={{ color: "#a63f04", width: 50 }} /> )}
+        </div>
+		</div>
 		{nPHD ? (
           <div className="row">
-            <div className="title">Ph.D Students</div>
             <div className="profiles">
               {allMembers
                 ?.filter(({ id, member }) => member.Position === "PhD")
@@ -233,9 +280,14 @@ function Team() {
         ) : (
           ""
         )}
+		<div className="row">
+		<div className="title" onClick={changeViewMtech}> Masters Students {"  "}
+		{nMtech ? (  <ExpandLessIcon style={{ color: "#a63f04", width: 50}}  /> ) : ( <ExpandMoreIcon style={{ color: "#a63f04", width: 50 }} /> )}
+        </div>
+		</div>
         {nMtech ? (
           <div className="row">
-            <div className="title">Master's Students</div>
+ 
             <div className="profiles">
               {allMembers
                 ?.filter(({ id, member }) => member.Position === "Masters")
@@ -318,9 +370,15 @@ function Team() {
         ) : (
           ""
         )}
+	
+		<div className="row">
+		<div className="title" onClick={changeViewBtech}> B.Tech Students {"  "}
+		{nBtech ? (  <ExpandLessIcon style={{ color: "#a63f04", width: 50}}  /> ) : ( <ExpandMoreIcon style={{ color: "#a63f04", width: 50 }} /> )}
+        </div>
+		</div>
         {nBtech ? (
           <div className="row">
-            <div className="title">B.Tech Students</div>
+
             <div className="profiles">
               {allMembers
                 ?.filter(({ id, member }) => member.Position === "BTech")
@@ -491,96 +549,64 @@ function Team() {
         ) : (
           ""
         )}
-        
-        {nAlumni ? (
+		
+		 <div className="row">		
+          <div className="title" onClick={changeViewCollab}> Collaborators {"  "}
+			  {viewCollab ? (  <ExpandLessIcon style={{ color: "#a63f04", width: 50}}  /> ) : ( <ExpandMoreIcon style={{ color: "#a63f04", width: 50 }} /> )}
+          </div>
+        </div>  	
+		{viewCollab ? (
           <div className="row">
-            <div className="title">Alumni</div>
             <div className="profiles">
               {allMembers
-                ?.filter(({ id, member }) => member.Position === "Alumni")
+                ?.filter(({ id, member }) => member.Position === "Collaborator")
                 .map(({ id, member }) => (
-                  <div className="container">
-                    <img src={member.ImageURL} alt={member.Name} />
+
+                  <div className="container2">
+
                     <div className="middle">
                       <div className="wrapper">
                         <div className="Title">
-                          {member.Name}
-                          <div className="interests">{member.Designation}</div>
-                        </div>
-
-                        <div className="socials">
 						  {member?.webpageLink !== "" ? (
                             <a
                               href={member?.webpageLink}
                               target="_blank"
                               rel="noreferrer"
                             >
-                              <Home />
+                              {member.Name}
                             </a>
                           ) : (
-                            <></>
+                            <> {member.Name} </>
                           )}
-                          {member?.twitterLink !== "" ? (
-                            <a
-                              href={member?.twitterLink}
-                              target="_blank"
-                              rel="noreferrer"
-                            >
-                              <Twitter />
-                            </a>
-                          ) : (
-                            <></>
-                          )}
+                        
+                          <div className="interests">{member.Designation}</div>
+                        
+                       </div>
 
-                          {member?.linkedinLink !== "" ? (
-                            <a
-                              href={member?.linkedinLink}
-                              target="_blank"
-                              rel="noreferrer"
-                            >
-                              <LinkedIn />
-                            </a>
-                          ) : (
-                            <></>
-                          )}
-                          {member?.githubLink !== "" ? (
-                            <a
-                              href={member?.githubLink}
-                              target="_blank"
-                              rel="noreferrer"
-                            >
-                              <GitHub />
-                            </a>
-                          ) : (
-                            <></>
-                          )}
-                          {member?.Mail !== "" ? (
-                            <a
-                              href={`mailto:${member?.Mail}`}
-                              target="_blank"
-                              rel="noreferrer"
-                            >
-                              <Mail />
-                            </a>
-                          ) : (
-                            <></>
-                          )}
-                        </div>
-                      </div>
+					  </div>
                     </div>
-                  </div>
+
+				  </div>
+
+
                 ))}
             </div>
           </div>
         ) : (
           ""
         )}
-        {viewCollab ? (
+
+		 <div className="row">		
+          <div className="title" onClick={changeViewAlumni}> Alumni {"  "}
+			  {nAlumni ? (  <ExpandLessIcon style={{ color: "#a63f04", width: 50}}  /> ) : ( <ExpandMoreIcon style={{ color: "#a63f04", width: 50 }} /> )}
+          </div>
+        </div>  
+        {nAlumni ? (
           <div className="row">
-            <div className="title">Collaborators</div>
-            <div className="profiles">
+           
+			<div className="profiles">
               {allMembers
-                ?.filter(({ id, member }) => member.Position === "Collaborator")
+                ?.filter(({ id, member }) => member.Position === "Alumni")
                 .map(({ id, member }) => (
                   <div className="container">
                     <img src={member.ImageURL} alt={member.Name} />
